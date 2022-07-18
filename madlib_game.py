1d73a1b9e2d51a9b23d78d72ea6fa3de3c396821
kelechistack12345 <57069824+kelechistack12345@users.noreply.github.com>
2022-07-17 22:48:29 -0700

    fourth commit

---
+++ madlib_game.py
@@ -0,0 +1,9 @@
+color = input ("Enter a color: ")
+plural_noun = input ("Enter a pural noun: ")
+celebrity = input ("Enter a celebrity: ")
+
+
+
+print("roses are "  + color)
+print( plural_noun + " are blue")
+print("i love "  +  celebrity)
