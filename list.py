911954c6a80ee79900ce2f842ccd6fe80435a6e1
kelechistack12345 <57069824+kelechistack12345@users.noreply.github.com>
2022-07-17 23:23:09 -0700

    fifth commit

---
+++ list.py
@@ -0,0 +1,23 @@
+#access element based of ther index
+friends=["kevin","karen","jim","kc","sammy"]
+print(friends[0])
+
+
+#access element from no 1 index to the last
+print(friends[1:])
+
+#access element from no 1 index to index 3
+print(friends[1:3])
+
+#modify element on 1 index 
+friends[1]="mike"
+print(friends[1])
+
+
+
+
+
+
+
+
+
