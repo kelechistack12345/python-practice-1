fbf1c16115cd7f510083f9b43a8bd38538aa82be
kelechistack12345 <57069824+kelechistack12345@users.noreply.github.com>
2022-07-18 15:08:44 -0700

    sixth commit

--- list.py
+++ list.py
@@ -1,4 +1,4 @@
-#access element based of ther index
+""" #access element based of ther index
 friends=["kevin","karen","jim","kc","sammy"]
 print(friends[0])
 
@@ -11,7 +11,24 @@ print(friends[1:3])
 
 #modify element on 1 index 
 friends[1]="mike"
-print(friends[1])
+print(friends[1]) """
+
+
+lucky_number = [4,8,5,6,12]
+friends = ["kevin","sammy","true","false","1"]
+print(friends)
+
+
+
+# to apphend you list
+# friends.extend(lucky_number)
+# print(friends)
+
+# lucky_number.extend(friends)
+# print(lucky_number)
+
+friends.append("creed")
+print(friends)
 
 
 
